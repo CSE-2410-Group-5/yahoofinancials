a8af7bb1fb0280a5e7b6f44345f06b8a8d41dd4f
Jordan Synodis <jsynodis2021@my.fit.edu>
2023-04-02 16:16:12 -0400

    error_checking

--- tKinterTest.py
+++ tKinterTest.py
@@ -4,6 +4,7 @@ import time
 from yahoofinancials import YahooFinancials as YF
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+import yfinance
 
 
 DEFAULT_ARGS = ('DOGE-JPY')
@@ -12,16 +13,16 @@ HELP_ARGS = ('-h', '--help')
 OUTPUT = ''
 mark = '-' * 64
 
-data = []
+tick = None
 
 def default_api(ticker):
     global OUTPUT
-    global data
+    global tick
+    
     tick = YF(ticker)
-
-    data = [tick.get_open_price(), tick.get_daily_low(), tick.get_daily_high(), tick.get_current_price()]
     
     OUTPUT += str(tick.get_summary_data()) + '\n'
+    '''
     OUTPUT += mark + '\n'
     OUTPUT += str(tick.get_stock_quote_type_data()) + '\n'
     OUTPUT += mark + '\n'
@@ -30,8 +31,7 @@ def default_api(ticker):
     OUTPUT += str(tick.get_current_price()) + '\n'
     OUTPUT += mark + '\n'
     OUTPUT += str(tick.get_dividend_rate()) + '\n'
-
-    display_stock_graph(root)
+    '''
 
     try:
         r = tick._cache.keys()
@@ -76,41 +76,51 @@ def test_button():
     global DEFAULT_ARGS
     global OUTPUT
     DEFAULT_ARGS = (first_entry.get())
-    
-    api = set(s for s in dir(YF) if s.startswith('get_'))
-    api.update(MODULE_ARGS)
-    api.update(HELP_ARGS)
-    ts = sys.argv[1:]
-    queries = [q for q in ts if q in api]
-    ts = [t for t in ts if not t in queries] or DEFAULT_ARGS
-    if [h for h in HELP_ARGS if h in queries]:
-        help_api(queries)
-    elif queries:
-        custom_api(queries, ts)
-    else:
-        timeit(default_api, ts[0] if 1 == len(ts) else ts)
 
-    text = tk.Text(root, wrap='word')
-    text.insert('insert', OUTPUT)
-    text.place(x=1, y=30)
-    root.update()
-    OUTPUT = ''
+    if(is_valid_ticker(DEFAULT_ARGS)):
+        api = set(s for s in dir(YF) if s.startswith('get_'))
+        api.update(MODULE_ARGS)
+        api.update(HELP_ARGS)
+        ts = sys.argv[1:]
+        queries = [q for q in ts if q in api]
+        ts = [t for t in ts if not t in queries] or DEFAULT_ARGS
+        if [h for h in HELP_ARGS if h in queries]:
+            help_api(queries)
+        elif queries:
+            custom_api(queries, ts)
+        else:
+            timeit(default_api, ts[0] if 1 == len(ts) else ts)
+        
+        text = tk.Text(root, wrap='word')
+        text.insert('insert', OUTPUT)
+        text.place(x=1, y=46, width=710, height=300)
+        root.update()
+        OUTPUT = ''
 
 
-def display_stock_graph(root):
+def display_stock_graph():
+    data = [tick.get_open_price(), tick.get_daily_low(), tick.get_daily_high(), tick.get_current_price()]
 
     # Create a figure and add a subplot
     fig = Figure(figsize=(5, 4), dpi=100)
     ax = fig.add_subplot(111)
 
-    print(data)
     # Plot the data as a line graph
     ax.plot(data)
 
     # Create a canvas to display the graph in Tkinter
     canvas = FigureCanvasTkAgg(fig, master=root)
     canvas.draw()
-    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    canvas.get_tk_widget().place(x=0, y=390)
+
+
+def is_valid_ticker(symbol):
+    try:
+        ticker = yfinance.Ticker(symbol)
+        info = ticker.info['regularMarketPrice']
+        return True
+    except:
+        return False
 
 
 # BEGIN TKINTER BUILD
@@ -126,29 +136,29 @@ currency_options = [
 
 root = tk.Tk()
 root.title("test")
-root.geometry("700x350")
+root.geometry("2000x900")
 
-first_label = tk.Label(root, text='Enter ticker here: ')
-first_label.pack(side='left', anchor='nw')
+first_label = tk.Label(root, text='Enter ticker here: ', font=('Times', 26))
+first_label.place(x=0, y=0)
 
-first_entry = tk.Entry(root)
-first_entry.pack(side='left', anchor='nw')
+first_entry = tk.Entry(root, font=('Times', 26))
+first_entry.place(x=250, y=0)
 
 # Create Dropdown menu for currency type
 clicked = tk.StringVar()  # datatype of menu text
 clicked.set("$")  # initial menu text
 currency_drop = tk.OptionMenu(root, clicked, *currency_options)
-currency_drop.config(width=6)
-currency_drop.pack(side='left', anchor='nw')
+currency_drop.config(height=2, width=6)
+currency_drop.place(x=585, y=0)
 
-search_bar_go_button = tk.Button(root, text='GO', command=lambda:test_button(), height = 1, width = 5)
-search_bar_go_button.place(x = '300', y = 0)
+search_bar_go_button = tk.Button(root, text='GO', command=lambda:test_button(), height = 2, width = 5)
+search_bar_go_button.place(x=665, y=3)
 
-quit_button = tk.Button(root, text='Quit', command=root.quit)
-quit_button.place(x = 1490, y = 0)
+quit_button = tk.Button(root, text='Quit', command=root.quit, height=2, width=5)
+quit_button.place(x = 1480, y = 0)
 
 # Graph button
-graph_button = tk.Button(root, text='Graph')
-graph_button.place(x=10, y=300)
+graph_button = tk.Button(root, text='Graph', command=lambda:display_stock_graph(), height = 2, width = 5)
+graph_button.place(x=1, y=348)
 
 root.mainloop()
