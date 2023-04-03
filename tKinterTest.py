7b92593b459ec6875cd3e4125988a749471f5362
Jordan Synodis <jsynodis2021@my.fit.edu>
2023-04-03 11:50:13 -0400

    updates

--- tKinterTest.py
+++ tKinterTest.py
@@ -21,25 +21,18 @@ def default_api(ticker):
     
     tick = YF(ticker)
     
-    OUTPUT += str(tick.get_summary_data()) + '\n'
-    '''
-    OUTPUT += mark + '\n'
-    OUTPUT += str(tick.get_stock_quote_type_data()) + '\n'
-    OUTPUT += mark + '\n'
-    OUTPUT += str(tick.get_stock_price_data()) + '\n'
-    OUTPUT += mark + '\n'
-    OUTPUT += str(tick.get_current_price()) + '\n'
-    OUTPUT += mark + '\n'
-    OUTPUT += str(tick.get_dividend_rate()) + '\n'
-    '''
+    OUTPUT += '{:25s}{:,f}\n'.format('Current Price: ', tick.get_current_price())
+    OUTPUT += '{:22s}{:,f}\n'.format('Current Volume: ', tick.get_current_volume())
+    OUTPUT += '{:23s}{:,f}\n'.format('Prev Close Price: ', tick.get_prev_close_price())
+    OUTPUT += '{:26s}{:,f}\n'.format('Open Price: ', tick.get_open_price())
+    OUTPUT += '{:26s}{:,f}\n'.format('Daily Low: ', tick.get_daily_low())
+    OUTPUT += '{:27s}{:,f}\n'.format('Daily High: ', tick.get_daily_high())
 
     try:
         r = tick._cache.keys()
     except AttributeError:
         pass
-    else:
-        OUTPUT += mark + '\n'
-        OUTPUT += str(r) + '\n'
+
 
 def custom_api(queries, ts):
     global OUTPUT
@@ -64,38 +57,45 @@ def help_api(queries):
 
 def timeit(f, *args):
     global OUTPUT
-    OUTPUT += mark + '\n'
     st = time.time()
     f(*args)
     et = time.time()
-    OUTPUT += mark + '\n'
-    print(et - st, 'seconds')
 
+
+def setup():
+    global OUTPUT
+    
+    api = set(s for s in dir(YF) if s.startswith('get_'))
+    api.update(MODULE_ARGS)
+    api.update(HELP_ARGS)
+    ts = sys.argv[1:]
+    queries = [q for q in ts if q in api]
+    ts = [t for t in ts if not t in queries] or DEFAULT_ARGS
+    if [h for h in HELP_ARGS if h in queries]:
+        help_api(queries)
+    elif queries:
+        custom_api(queries, ts)
+    else:
+        timeit(default_api, ts[0] if 1 == len(ts) else ts)
+    
+    text = tk.Text(root, wrap='word', font=('Times', 18))
+    text.insert('insert', OUTPUT)
+    text.place(x=1, y=46, width=710, height=300)
+    root.update()
+    OUTPUT = ''
 
 def test_button():
-    global DEFAULT_ARGS
-    global OUTPUT
+    global DEFAULT_ARGS 
     DEFAULT_ARGS = (first_entry.get())
 
     if(is_valid_ticker(DEFAULT_ARGS)):
-        api = set(s for s in dir(YF) if s.startswith('get_'))
-        api.update(MODULE_ARGS)
-        api.update(HELP_ARGS)
-        ts = sys.argv[1:]
-        queries = [q for q in ts if q in api]
-        ts = [t for t in ts if not t in queries] or DEFAULT_ARGS
-        if [h for h in HELP_ARGS if h in queries]:
-            help_api(queries)
-        elif queries:
-            custom_api(queries, ts)
+        setup()
+    else:
+        DEFAULT_ARGS += '-' + clicked.get()
+        if(is_valid_ticker(DEFAULT_ARGS)):
+            setup()
         else:
-            timeit(default_api, ts[0] if 1 == len(ts) else ts)
-        
-        text = tk.Text(root, wrap='word')
-        text.insert('insert', OUTPUT)
-        text.place(x=1, y=46, width=710, height=300)
-        root.update()
-        OUTPUT = ''
+            tk.messagebox.showinfo('Invalid Ticker', 'The option you entered is not a valid ticker. Please try again.')
 
 
 def display_stock_graph():
@@ -121,6 +121,44 @@ def is_valid_ticker(symbol):
         return True
     except:
         return False
+    
+# Function to toggle the visibility of the listbox when the entry widget is clicked
+def toggle_dropdown(event):
+    global dropdown_visible
+    if dropdown_visible:
+        listbox.pack_forget()
+        dropdown_visible = False
+    else:
+        listbox.pack()
+        dropdown_visible = True
+
+# Function to update the selected options when the user makes a selection
+def update_selection():
+    global selected_options, dropdown_visible
+    selected_options = [listbox.get(idx) for idx in listbox.curselection()]
+    dropdown_entry_var.set(", ".join(selected_options))
+    dropdown_visible = False
+    listbox.pack_forget()
+
+    additional_text = ''
+
+    for i in selected_options:
+        if(i == 'Dividend Yield'):
+            temp = tick.get_dividend_yield()
+            if(temp == None):
+                temp = 'N/A'
+            additional_text += 'Dividend Yield: {}\n'.format(temp)
+        elif(i == 'Dividend Rate'):
+            temp = tick.get_dividend_rate()
+            if(temp == None):
+                temp = 'N/A'
+            additional_text += 'Dividend Rate: {}\n'.format(temp)
+
+    text2 = tk.Text(root, wrap='word', font=('Times', 18))
+    text2.insert('insert', additional_text)
+    # y was 46
+    text2.place(x=720, y=100, width=710, height=300)
+    root.update()
 
 
 # BEGIN TKINTER BUILD
@@ -128,8 +166,14 @@ def is_valid_ticker(symbol):
 # Holds data for drop down menus
 currency_options = [
     "USD",
-    "CAN",
+    "CAD",
     "EUR"
+]
+
+additional_info = [
+    "Dividend Yield",
+    "Dividend Rate",
+    "Split History"
 ]
 
 
@@ -146,13 +190,45 @@ first_entry.place(x=250, y=0)
 
 # Create Dropdown menu for currency type
 clicked = tk.StringVar()  # datatype of menu text
-clicked.set("$")  # initial menu text
+clicked.set(currency_options[0])  # initial menu text
 currency_drop = tk.OptionMenu(root, clicked, *currency_options)
 currency_drop.config(height=2, width=6)
 currency_drop.place(x=585, y=0)
 
 search_bar_go_button = tk.Button(root, text='GO', command=lambda:test_button(), height = 2, width = 5)
 search_bar_go_button.place(x=665, y=3)
+
+
+# Additional information text
+stock_data_label = tk.Label(root, text='Additional Information: ', font=('Times', 26))
+stock_data_label.place(x=720, y=0)
+
+# Additional Information Dropdown menu
+# Create a frame to hold the dropdown widget
+dropdown_frame = tk.Frame(root)
+dropdown_frame.place(x=1058, y=0)
+# Create an entry widget to display the selected options
+dropdown_entry_var = tk.StringVar()
+dropdown_entry_var.set("Select options...")
+dropdown_entry = tk.Entry(dropdown_frame, textvariable=dropdown_entry_var, width=14, font=('Times', 26))
+dropdown_entry.pack()
+
+# Create a listbox widget to display the options when the dropdown is opened
+dropdown_visible = False
+listbox = tk.Listbox(dropdown_frame, selectmode=tk.MULTIPLE, font=('Times', 18))
+for option in additional_info:
+    listbox.insert(tk.END, option)
+listbox.pack_forget()
+
+# Bind events to the entry and listbox widgets to handle the dropdown behavior
+dropdown_entry.bind("<Button-1>", toggle_dropdown)
+listbox.bind("<FocusOut>", lambda event: dropdown_entry.focus())
+listbox.bind("<Button-1>", lambda event: dropdown_entry.focus())
+listbox.bind("<ButtonRelease-1>", lambda event: update_selection)
+
+add_info_go_button = tk.Button(root, text='GO', command=update_selection, height=2, width=5)
+add_info_go_button.place(x='1315', y=3)
+
 
 quit_button = tk.Button(root, text='Quit', command=root.quit, height=2, width=5)
 quit_button.place(x = 1480, y = 0)
